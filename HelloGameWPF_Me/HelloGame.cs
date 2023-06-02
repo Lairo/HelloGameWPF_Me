610f12921f458c82d3e939308a65d3d90353c5b9
chajus1 <chajus1@googlemail.com>
2023-06-02 03:32:43 +0100

    Add project files.

---
+++ HelloGameWPF_Me/HelloGame.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    internal static class HelloGame
+    {
+        public const int MAXIMUM = 10;
+        private static Random random = new Random();
+        static private int currentNumber = random.Next(1, MAXIMUM + 1);
+        static private int nextNumber = random.Next(1, MAXIMUM + 1);
+        static private int pot = 10;
+
+        internal static void Cheat()
+        {
+            random = new Random(1);
+            Random cheatRandom = new Random(1);
+            Console.WriteLine("The first 20 numbers will be: ");
+
+            for (int i = 0; i < 20; i++)
+            {
+                Console.Write(cheatRandom.Next(1, MAXIMUM + 1) + " ");
+            }
+        }
+
+        internal static int GetPot(){ return pot; }
+
+        internal static void Guess(bool higher)
+        {
+            if ((nextNumber >= currentNumber && higher) || (nextNumber <= currentNumber && !higher))
+            {
+                Console.WriteLine("\nYou guessed right!");
+                pot++;
+            
+            } else
+            {
+                Console.WriteLine("\nBad luck, you guessed wrong.");
+                pot--;
+            }
+
+            currentNumber = nextNumber;
+            nextNumber = random.Next(1, MAXIMUM + 1);
+            Console.WriteLine($"\nThe current number is {currentNumber}.");
+        }
+
+        internal static void Hint()
+        {
+            float half = (float)nextNumber / 2;
+            Console.WriteLine($"\nThe current number is {currentNumber}, the next is at least {half}.");
+            pot--;
+        }
+    }
+}
